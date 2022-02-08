ce9f8b9f4c3c002e3a994d3dfc06b5c7c2ab41b0
Marshall <marshallmassengill@gmail.com>
2022-02-07 20:53:23 -0500

    added script and reg files, broke cmake for Henry

--- elf2uf2/cmake_install.cmake
+++ elf2uf2/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/henry/code/pico-sdk/tools/elf2uf2
+# Install script for directory: /opt/pico-sdk/tools/elf2uf2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -37,14 +37,9 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/henry/code/pico16470/elf2uf2/boot_uf2_headers/cmake_install.cmake")
+  include("/home/ubuntu/pico16470/elf2uf2/boot_uf2_headers/cmake_install.cmake")
 
 endif()
 
@@ -56,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/henry/code/pico16470/elf2uf2/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ubuntu/pico16470/elf2uf2/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
